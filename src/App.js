2a12786d0dd1d8cf4166a2b9315824c9c986094c
Cody Boyko <codyboyko@yahoo.ca>
2017-08-14 23:40:03 +0000

    Add create contact button to change state.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,13 @@ class App extends Component {
     return (
       <div>
         {this.state.screen === 'list' && (
-          <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact} />
+          <ListContacts
+            contacts={this.state.contacts} 
+            onDeleteContact={this.removeContact} 
+            onNavigate={() => {
+              this.setState({screen: 'create'})
+            }}
+          />
         )}
         {this.state.screen === 'create' && (
           <CreateContact />
